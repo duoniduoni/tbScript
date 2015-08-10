68ca2187cc6de32c384d42db04dbdd645c646aba
zhangsong-T410 <duoniduoni@126.com>
2015-08-10 10:33:17 +0800

    测试 字符编码出问题的情况

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 ant -buildfile /home/zhoangsong/workspace/Demo1
 adb push /home/zhoangsong/workspace/Demo1/bin/taobaotest.jar /data/local/tmp/
-adb shell uiautomator runtest taobaotest.jar -c com.uiautomatortest.Test#testDemo2 -e args 泳镜*Lining#李宁#游泳镜男#防水#防雾#高清*TRUE*30*20
+adb shell uiautomator runtest taobaotest.jar -c com.uiautomatortest.Test#testDemo2 -e args 单人沙发*简易单人#地中海#欧式#田园*TRUE*30*20
